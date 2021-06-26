bbacf631dd37bdb85aefafc1390c5c54314afa63
Danilo Chang <ray2501@gmail.com>
2021-06-26 12:42:02 +0800

    Update to 0.8

--- build.tcl
+++ build.tcl
@@ -1,9 +1,9 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "tksvg-0.7"
+set base "tksvg-0.8"
 
-set fileurl "https://github.com/oehhar/tksvg/archive/0.7.tar.gz"
+set fileurl "https://github.com/oehhar/tksvg/archive/0.8.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
