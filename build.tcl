4da6d5f54a8082f7027972e3b3c71e0e4f94c601
Danilo Chang <ray2501@gmail.com>
2023-07-06 19:26:26 +0800

    Update to 0.13

--- build.tcl
+++ build.tcl
@@ -1,9 +1,9 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "tksvg-0.12"
+set base "tksvg-0.13"
 
-set fileurl "https://github.com/oehhar/tksvg/archive/0.12.tar.gz"
+set fileurl "https://github.com/oehhar/tksvg/archive/0.13.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
--- tksvg.spec
+++ tksvg.spec
@@ -4,7 +4,7 @@
 
 Name:          tksvg
 Summary:       Read the SVG image format from Tk
-Version:       0.12
+Version:       0.13
 Release:       0
 License:       BSD-3-Clause
 Group:         Development/Libraries/Tcl
